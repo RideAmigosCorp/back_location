1b2670710c8d716ec691829689c7ee60b360a23a
Florian Daniel <florian@rideamigos.com>
2023-07-04 13:25:17 +0200

    Fix/paths (#2)
    * fix: paths
    
    * fix: platform path
    
    * fix: another path
    
    * fix: another path
    
    * chore: remove not needed dart classes

--- packages/back_location_android/lib/back_location_android.dart
+++
@@ -1,9 +0,0 @@
-import 'package:back_location_platform_interface/back_location_platform_interface.dart';
-
-/// The Android implementation of [LocationPlatform].
-class BackLocationAndroid {
-  /// Registers this class as the default instance of [LocationPlatform]
-  static void registerWith() {
-    LocationPlatform.instance = MethodChannelLocation();
-  }
-}
